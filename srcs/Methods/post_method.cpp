4999de2f5ba1d8677635da321112018c0d62a42c
Danae Duvivier <Danae.Duvivier@ulb.ac.be>
2023-08-16 02:14:35 +0200

    wip. need to find the issue with location

--- srcs/Methods/post_method.cpp
+++ srcs/Methods/post_method.cpp
@@ -12,26 +12,65 @@
 		- Return the http code (int) that you can use for build response. For exemple, if the upload was sucessfull that's return 200
 */
 
+std::string	get_absolute_path(Server& server, std::string path, std::string root)
+{
+	std::string	fullPath;
+
+	if (!root.empty())
+		fullPath = root + path;
+	else
+		fullPath = server.getRoot() + path;
+	return (fullPath);
+}
+
 void	handlePost(Server& server, Request& request, Response& response)
 {
 	int 		code;
 	Location	location;
-	std::string	directory;
+	std::string	fullPath;
+	std::string	uploadstore;
 	std::string path = request.getPath();
 
-	location = server.getLocationByPath(path);
-	directory = location.getUploadStore();
+	if (server.locationExist(path))
+	{
+		location = server.getLocationByPath(path);
+		if (!location.getAllowedMethods("POST"))
+		{
+			response.createResponse("405", server);
+			return ;
+		}
+		// -------- LOCATION ISSUE ------------
+		std::cout << "Location: " << location << std::endl;
+		uploadstore = location.getUploadStore();
+		std::cout << "uploadstore: " << uploadstore << std::endl;
+		if (uploadstore.empty())
+			fullPath = get_absolute_path(server, path, location.getRoot());
+		else
+			fullPath = get_absolute_path(server, uploadstore, location.getRoot());
+	}
+	else
+	{
+		std::cout << "LOCATION NOT EXIST" << std::endl;
+		if (!server.getAllowedMethods("POST"))
+		{
+			response.createResponse("405", server);
+			return ;
+		}
+		fullPath = get_absolute_path(server, path, "");
+	}
+
+	std::cout << "fullPath: " << fullPath << std::endl;
+
 	if (request.isUpload())
 	{
-		if (directory.empty())
-			directory = path;
-		code = createBinaryFileFromBody(request, directory);
+		code = createBinaryFileFromBody(request, fullPath);
+		std::cout << "code: " << code << std::endl;
 		response.createResponse(to_string(code), server);
 		return;
 	}
 	else
 	{
-		std::cout << "RECEIVED DATA FROM CLIENT" << std::endl;
-		// work in progress
+		//parse_data(request.getBody());
+		response.createResponse("204", server);
 	}
 }
